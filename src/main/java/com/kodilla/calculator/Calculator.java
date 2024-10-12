6335f15b9e895a224fffdf1e71c6b042290fe0ae
Paweł Kosiorek <48069213+PawelKosiorek@users.noreply.github.com>
2024-10-12 11:52:45 +0200

    first commit

---
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.calculator;
+
+class Calculator {
+
+    public int subtractTwoNum(int a, int b) {
+        return a - b;
+    }
+    public int addTwoNum(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ src/main/java/com/kodilla/calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.calculator;
+
+
+
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		calculator.addTwoNum(11, 17);
+		calculator.subtractTwoNum(12, 23);
+	}
+
+}
